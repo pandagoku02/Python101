686f59cc2f7722ced02f4f33207989af6d5f417f
pandagoku02 <73413745+pandagoku02@users.noreply.github.com>
2024-12-27 21:07:39 +0530

    Add files via upload

---
+++ 3-taking input.py
@@ -0,0 +1,23 @@
+inp = input('Hello')
+inp2= input("Enter a value:\n")
+print(inp2)
+
+# prgram to check the input type
+
+num = input('Enter number ')
+print(num)
+name1 = input('Enter name:')
+print(name1)
+
+#printing the type of input
+print('Type of number',type(num))
+print('Type of name',type(name1))
+
+#-----------program to take number only input
+
+num2 = int(input('Enter an integer number:'))
+print(num2,"  ", type(num2))
+
+num3 = float(input('Enter a decimal number:'))
+print(num3, "  ", type(num3))
+print()
